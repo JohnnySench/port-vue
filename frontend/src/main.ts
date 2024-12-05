b1679a596c7568c55d55310cde8f0e235a822305
Evgeniy Senchukov <evgenii.senchukov@dalee.ru>
2024-12-05 14:43:13 +0300

    add initial config

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './assets/style/main.pcss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,3 +1,5 @@
+<script setup lang="ts"></script>
+
 <template>
   <div class="about">
     <h1>Hello ДЖОН!!!!</h1>
