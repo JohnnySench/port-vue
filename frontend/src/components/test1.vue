6e4bbdb4212b4b3122fae306dfbbe252b739ffb7
Evgeniy Senchukov <evgenii.senchukov@dalee.ru>
2025-01-21 15:00:22 +0300

    add feat

--- frontend/src/components/test1.vue
+++ frontend/src/components/test1.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 console.log(1)
 console.log(2)
+console.log(2)
 import { ref, toRef, markRaw } from "vue"
 const props = defineProps({ value: Boolean })
 const defaultValue = toRef(() => props.value)
